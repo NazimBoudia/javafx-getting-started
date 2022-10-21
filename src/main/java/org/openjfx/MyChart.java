cac98c5ee108fb5e8045569d19f80e4e2bd9f59e
Nazim <nazim.boudia@gmail.com>
2022-10-21 10:58:36 +0200

    MyChart and MyHeader class : - Random numbers on the graph

--- src/main/java/org/openjfx/MyChart.java
+++ src/main/java/org/openjfx/MyChart.java
@@ -5,9 +5,19 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.VBox;
 
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.util.Random;
+
 public class MyChart extends VBox{
+    XYChart.Series<Double, Double> series = new XYChart.Series<>();
+    LineChart lc = new LineChart(
+            new NumberAxis("Time Constant ", 0.0, 15.0, 1),
+            new NumberAxis("Voltage(Vs) ", 0.0, 1.0, 0.1)
+    );
     public LineChart buildSampleLineChart() {
-        XYChart.Series<Double, Double> series = new XYChart.Series<>();
         series.getData().add(new XYChart.Data<>(0.0, 0.0));
         series.getData().add(new XYChart.Data<>(0.7, 0.5));
         series.getData().add(new XYChart.Data<>(1.0, 0.632));
@@ -15,15 +25,22 @@ public class MyChart extends VBox{
         series.getData().add(new XYChart.Data<>(3.0, 0.95));
         series.getData().add(new XYChart.Data<>(4.0, 0.982));
         series.getData().add(new XYChart.Data<>(5.0, 0.993));
-        LineChart lc = new LineChart(
-                new NumberAxis("Time Constant ", 0.0, 5.0, 1),
-                new NumberAxis("Voltage(Vs) ", 0.0, 1.0, 0.1)
-        );
         lc.getData().add(series);
         return lc;
 
     }
    public MyChart(){
         getChildren().add(buildSampleLineChart());
+        Button myButton = new Button("Add random value");
+        myButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent){
+                Random rand = new Random();
+                double randomT = 5*rand.nextDouble();
+                double randomV = rand.nextDouble();
+                series.getData().add(new XYChart.Data<>(randomT,randomV));
+           }
+        });
+        getChildren().add(myButton);
     }
 }
--- src/main/java/org/openjfx/MyHeader.java
+++ src/main/java/org/openjfx/MyHeader.java
@@ -1,5 +1,6 @@
 package org.openjfx;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 
